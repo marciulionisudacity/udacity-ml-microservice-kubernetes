28b005447db72048b81c63a86c35ce5c9116a964
tomas march <marciulionis.tomas+udacity@gmail.com>
2023-11-24 23:05:51 +0200

    Add docker_out.txt

--- run_docker.sh
+++ run_docker.sh
@@ -1,24 +1,7 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
-# Build image and add a descriptive tag
-
-# Step 2: 
-# List docker images
-
-# Step 3: 
-# Run flask app
-
-
-#!/usr/bin/env bash
-
 # Build image
 docker build --tag=udacity-ml-microservice .
-
-echo "Successfully tagged udacity-ml-microservice container"
-echo "Successfully built udacity-ml-microservice container"
 
 # List docker images
 docker image ls
