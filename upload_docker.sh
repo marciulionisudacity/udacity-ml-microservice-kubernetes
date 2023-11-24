16d41ae5f3642d21f4dd01f7a1500cd1b9cf2903
tomas march <marciulionis.tomas+udacity@gmail.com>
2023-11-24 23:32:39 +0200

    Introduce upload script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerimage=udacity-ml-microservice
+dockerpath=tomasmarch/$dockerimage
 
 # Step 2:  
 # Authenticate & tag
+docker tag $dockerimage $dockerpath:v1.0.0
+docker login
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:v1.0.0
