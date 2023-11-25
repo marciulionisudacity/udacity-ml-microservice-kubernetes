cc5b5517a632261d9cbde688863fc7ae95e65a07
tomas march <marciulionis.tomas+udacity@gmail.com>
2023-11-25 21:12:55 +0200

    Fix

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
+dockerpath="tomasmarch/udacity-ml-microservice"
 
-# This tags and uploads an image to Docker Hub
+# Run in Docker Hub container with kubernetes
+kubectl run udacitymlmicroservice\
+    --image=$dockerpath 
+    --port=80 
+    --labels app=udacitymlmicroservice
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
-# Step 2
-# Run the Docker Hub container with kubernetes
-
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
-# Forward the container port to a host
+# Forward the container port to host
+kubectl port-forward udacitymlmicroservice 8080:80
 
