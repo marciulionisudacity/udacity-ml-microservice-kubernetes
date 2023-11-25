3cff596c3753a2b1682297ad13926f89b7deb0b9
tomas march <marciulionis.tomas+udacity@gmail.com>
2023-11-25 21:24:14 +0200

    Fix

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-dockerpath="tomasmarch/udacity-ml-microservice"
+dockerpath="tomasmarch/udacity-ml-microservice:v1.0.0"
 
 # Run in Docker Hub container with kubernetes
-kubectl run udacitymlmicroservice\
-    --image=$dockerpath 
-    --port=80 
-    --labels app=udacitymlmicroservice
+kubectl run udacitymlmicroservice --image=$dockerpath --port=80 --labels app=udacitymlmicroservice
+
+# Wait for the pod to be in the "Running" state
+while [[ $(kubectl get pods udacitymlmicroservice -o 'jsonpath={.status.phase}') != "Running" ]]; do
+    echo "Waiting for the pod to be in the 'Running' state..."
+    sleep 5
+done    
 
 # List kubernetes pods
 kubectl get pods
